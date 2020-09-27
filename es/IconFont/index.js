ffb5bde580d4eb99bc1dd5922367d01a79e7b0b8
古月 <guyue@jjjj.local>
2020-09-27 11:43:44 +0800

    删除多余文件

--- es/IconFont/index.js
+++
@@ -1,6 +0,0 @@
-import { createFromIconfontCN } from '@ant-design/icons';
-var Index = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1671065_9j52mtuhkew.js' // 在 iconfont.cn 上生成
-
-});
-export default Index;
