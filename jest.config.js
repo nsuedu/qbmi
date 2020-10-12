1cfdea331b57fe38229571a6ebab250bfdc1b743
huhao <huhao0821@outlook>
2020-10-12 22:09:44 +0800

    fix

--- jest.config.js
+++ jest.config.js
@@ -6,20 +6,21 @@ const pkgList = readdirSync(join(__dirname, './src')).filter((pkg) => pkg.charAt
 const moduleNameMapper = {};
 
 pkgList.forEach((shortName) => {
-  const name = `${shortName}`;
+  const name = `@ant-design/pro-${shortName}`;
   moduleNameMapper[name] = join(__dirname, `./src/${shortName}`);
 });
 
 module.exports = {
-  // collectCoverageFrom: [
-  //   'src/**/*.{ts,tsx}',
-  //   '!src/demos/**',
-  // ],
-  // moduleNameMapper,
+  collectCoverageFrom: [
+    'packages/**/src/**/*.{ts,tsx}',
+    '!packages/**/src/demos/**',
+    '!packages/**/src/component/ColumnSetting/DndItem.tsx',
+  ],
+  moduleNameMapper,
   testURL: 'http://localhost',
   verbose: true,
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
-  // extraSetupFiles: ['./tests/setupTests.js'],
+  extraSetupFiles: ['./tests/setupTests.js'],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
   },
--- tests/no-duplicated.ts
+++ tests/no-duplicated.ts
@@ -1,3 +1,8 @@
 // make sure no duplicated export interface
-export * from '../src/Select/index';
-export * from '../src/Tree';
+export * from '../packages/card/src';
+export * from '../packages/descriptions/src';
+export * from '../packages/form/src';
+export * from '../packages/layout/src';
+export * from '../packages/list/src';
+export * from '../packages/skeleton/src';
+export * from '../packages/table/src';
